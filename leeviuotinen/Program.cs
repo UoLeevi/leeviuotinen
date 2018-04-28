347f547f6bb0c33a74101034080b524cc61d0f15
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-04-28 20:25:38 +0300

    Add Empty ASP.NET Core project

---
+++ leeviuotinen/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace leeviuotinen
+{
+    public class Program
+    {
+        public static void Main(
+            string[] args)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseConfiguration(config)
+                .Build();
+
+            host.Run();
+        }
+    }
+}
