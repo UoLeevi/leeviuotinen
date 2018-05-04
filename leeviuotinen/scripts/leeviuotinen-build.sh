59cc3219004a0e814ed3e6cfad142607b44bc8ff
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-05-04 18:30:56 +0300

    Modify leeviuotinen.build.sh not to fail if directory leeviuotinen-app doesn't exist

--- leeviuotinen/scripts/leeviuotinen-build.sh
+++ leeviuotinen/scripts/leeviuotinen-build.sh
@@ -3,7 +3,7 @@ PID=$(ps aux | grep '[d]otnet leeviuotinen.dll' | awk '{print $2}')
 if [ ! -z "$PID" ]; then while sudo kill $PID; do sleep 1; done; fi
 unset PID
 cd /var/aspnetcore/leeviuotinen/leeviuotinen
-sudo rm -r /var/aspnetcore/leeviuotinen-app
+sudo rm -fr /var/aspnetcore/leeviuotinen-app
 sudo dotnet publish -o /var/aspnetcore/leeviuotinen-app -c Release
 cd /var/aspnetcore/leeviuotinen-app
 sudo rm -r /var/aspnetcore/leeviuotinen
