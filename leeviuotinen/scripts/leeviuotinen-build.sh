b9fe6ddc15f282c75c3d39d70003244c31b8ae8b
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-05-04 18:25:03 +0300

    Move logic to do git clone to Jenkins and make Jenkins execute new version of leeviuotinen-build.sh

--- leeviuotinen/scripts/leeviuotinen-build.sh
+++ leeviuotinen/scripts/leeviuotinen-build.sh
@@ -2,12 +2,8 @@ unset PID
 PID=$(ps aux | grep '[d]otnet leeviuotinen.dll' | awk '{print $2}')
 if [ ! -z "$PID" ]; then while sudo kill $PID; do sleep 1; done; fi
 unset PID
-
-cd /var/aspnetcore
-sudo rm -fr leeviuotinen
-sudo rm -fr leeviuotinen-app
-sudo git clone https://github.com/UoLeevi/leeviuotinen.git
-cd leeviuotinen/leeviuotinen
+cd /var/aspnetcore/leeviuotinen/leeviuotinen
+sudo rm -r /var/aspnetcore/leeviuotinen-app
 sudo dotnet publish -o /var/aspnetcore/leeviuotinen-app -c Release
 cd /var/aspnetcore/leeviuotinen-app
 sudo rm -r /var/aspnetcore/leeviuotinen
