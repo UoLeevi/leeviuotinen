8eb2e41c401a52dbb534aebe281cb97934f1b186
Leevi Uotinen <uotinen.leevi@hotmail.com>
2018-05-05 11:10:06 +0300

    Modify application to monitored by a service

--- leeviuotinen/scripts/leeviuotinen-build.sh
+++ leeviuotinen/scripts/leeviuotinen-build.sh
@@ -1,10 +1,17 @@
+sudo systemctl stop leeviuotinen.service
+sudo rm -f /etc/systemd/system/leeviuotinen.service
+
 unset PID
 PID=$(ps aux | grep '[d]otnet leeviuotinen.dll' | awk '{print $2}')
 if [ ! -z "$PID" ]; then while sudo kill $PID; do sleep 1; done; fi
 unset PID
+
 cd /var/aspnetcore/leeviuotinen/leeviuotinen
+sudo cp system/leeviuotinen.service /etc/systemd/system/
 sudo rm -fr /var/aspnetcore/leeviuotinen-app
 sudo dotnet publish -o /var/aspnetcore/leeviuotinen-app -c Release
+
 cd /var/aspnetcore/leeviuotinen-app
 sudo rm -r /var/aspnetcore/leeviuotinen
-sudo dotnet leeviuotinen.dll &
+
+sudo systemctl start leeviuotinen.service
